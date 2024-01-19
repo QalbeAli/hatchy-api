0e01fddd179de883969831247975543373e49af3
alan-palacios <apyolt@gmail.com>
2024-01-18 20:15:43 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+import { config } from "dotenv";
+config();
+import MastersHandler from "./src/masters/updateMastersPFPImage.js";
+import express, { json } from "express";
+
+const app = express();
+app.use(json());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server Listening on PORT:", PORT);
+});
+
+app.post("/masters/pfp/image/:tokenId", MastersHandler);
---
+++ src/masters/updateMastersPFPImage.js
@@ -0,0 +1,56 @@
+import axios from "axios";
+import mergeImages from "merge-images";
+import { Canvas, Image } from "canvas";
+
+const handler = async (request, response) => {
+  const tokenId = request.params.tokenId;
+
+  const url = `${process.env.API_URL}/masters/pfp/image-upload-url/${tokenId}`;
+  try {
+    const result = await axios.get(url, {
+      headers: {
+        Authorization: `Bearer ${process.env.API_KEY}`,
+      },
+    });
+    const { uploadUrl, traits } = result.data;
+    const b64 = await joinImages(
+      traits.map((trait) => trait.image).filter((image) => !!image)
+    );
+    await uploadImage(b64, uploadUrl);
+
+    response.send({
+      message: "Image uploaded successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    response.send({
+      message: "Image upload failed",
+    });
+  }
+};
+
+const joinImages = async (images) => {
+  console.log(images);
+  const b64 = await mergeImages(images, {
+    Canvas: Canvas,
+    Image: Image,
+  });
+  return b64;
+};
+
+const uploadImage = async (base64Image, uploadUrl) => {
+  const buffer = Buffer.from(
+    base64Image.replace(/^data:image\/\w+;base64,/, ""),
+    "base64"
+  );
+
+  await axios.put(uploadUrl, buffer, {
+    headers: {
+      "Content-Type": "image/png",
+      "Content-Encoding": "base64",
+    },
+  });
+  console.log("Image uploaded successfully ");
+};
+
+export default handler;
